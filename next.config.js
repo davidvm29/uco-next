31c5141fbc17856661d1df5c13fda8b25ef372d4
David Villen Moreno <dvillenmoreno@gmail.com>
2024-05-10 21:30:24 +0200

    finis

--- next.config.js
+++ next.config.js
@@ -6,6 +6,26 @@ const nextConfig = {
   output: 'export',
   basePath: isProd ? '/ff' : '',
   assetPrefix: isProd ? '/ff/' : '',
+  images: {
+    // Habilitar optimizaciones de imágenes automáticas
+    loader: 'default',
+    // Habilitar la optimización progresiva (escaneo de línea por línea) de imágenes JPEG
+    formats: ['image/jpeg', 'image/png'],
+    // Habilitar la carga diferida de imágenes
+    // lazy: true,
+    // Habilitar la priorización automática de formatos de imagen modernos (WebP) en navegadores compatibles
+    // preferWebp: true,
+    // Habilitar la compresión de imágenes en tiempo de compilación (se requiere el paquete sharp)
+    // optimizeImages: true,
+    // Habilitar la adaptación de imágenes de acuerdo con el tamaño del diseño del componente
+    // deviceSizes: [320, 420, 768, 1024, 1200],
+    // Habilitar la generación de imágenes de varias resoluciones para soporte de pantallas de alta densidad
+    // iconSizes: [],
+    // Habilitar la configuración de dominios para servir imágenes de origen cruzado
+    // domains: ['example.com', 'cdn.example.com'],
+    // Habilitar la configuración de rutas de imágenes personalizadas
+    // path: 'https://example.com/myImage/',
+  },
  
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
